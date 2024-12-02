efdb785f57434910db39883665c16d8783df374d
Matias Donoso <rmttck@gmail.com>
2024-12-02 09:44:57 -0300

    not connected mitigation

--- src/renderer/src/pages/PCBoxPage.tsx
+++ src/renderer/src/pages/PCBoxPage.tsx
@@ -32,31 +32,37 @@ function PCBoxPage(): JSX.Element {
 
   return (
     <div className="bg-slate-200 flex flex-col items-center justify-start w-full h-full p-4 overflow-hidden">
-  <h1 className="text-2xl font-bold mb-4 text-gray-800">PC BOX</h1>
-  {pcBoxPokemons.length === 0 ? (
-    <p className="text-xl font-semibold text-gray-600 text-center">
-    Your PC Box is empty. <br/>
-    Start catching more Pokémon, and they'll be sent here automatically!
-  </p>  
-  ) : (
-    <div className="w-full px-2 flex-grow overflow-auto custom-scrollbar">
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4">
-        {pcBoxPokemons.map((pokemon, index) => (
-          <PokemonCard
-            key={index}
-            name={pokemon.name}
-            image={pokemon.image}
-            types={pokemon.types}
-            description={pokemon.description}
-            moves={pokemon.moves}
-          />
-        ))}
-      </div>
+      <h1 className="text-2xl font-bold mb-4 text-gray-800">PC BOX</h1>
+      {!Array.isArray(pcBoxPokemons) ? (
+        <p className="text-xl font-semibold text-red-600 text-center">
+          Unable to connect to the database. <br />
+          Please verify that the user exists, has the necessary permissions, and that the database has been created according to the file located at: <br />
+          <code className="text-gray-800">src\backend\models\postgres.sql</code>
+        </p>
+      ) : pcBoxPokemons.length === 0 ? (
+        <p className="text-xl font-semibold text-gray-600 text-center">
+          Your PC Box is empty. <br />
+          Start catching more Pokémon, and they'll be sent here automatically!
+        </p>
+      ) : (
+        <div className="w-full px-2 flex-grow overflow-auto custom-scrollbar">
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4">
+            {pcBoxPokemons.map((pokemon, index) => (
+              <PokemonCard
+                key={index}
+                name={pokemon.name}
+                image={pokemon.image}
+                types={pokemon.types}
+                description={pokemon.description}
+                moves={pokemon.moves}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )}
-</div>
-
-  )
+  );
+  
 }
 
 export default PCBoxPage
